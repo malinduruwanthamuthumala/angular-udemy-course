99b7d2caff95c2c77040cd22bba463e13d3f606e
unknown <malinduruwantha@gmail.com>
2019-02-25 17:26:14 +0530

    two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,12 @@ import { Component ,OnInit} from '@angular/core';
 export class AppComponent {
 name='';
 usercreation="No user is registered";
-
+fname="nilakura";
 allowtosubmit=false;
+num1=0;
+num2=0;
+total=0;
+
 constructor(){
   setTimeout(()=>{
     this.allowtosubmit=true;
@@ -22,5 +26,12 @@ ngOnInit(){
 
 onCreateServer(){
   this.usercreation="user is created";
+}
+onUpdate(event: Event){
+this.fname=(<HTMLInputElement>event.target).value;
+}
+calculate(){
+ this.total=this.num1+this.num2;
+
 }
 }
