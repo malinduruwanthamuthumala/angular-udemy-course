d132382e575db22d6e7b58ba0b022fb376d72dcd
unknown <malinduruwantha@gmail.com>
2019-04-10 19:51:44 +0530

    added recepie list

--- src/app/recepie/recepie-list/recepie-list.component.ts
+++ src/app/recepie/recepie-list/recepie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recepie } from '../recepie.model';
 
 @Component({
   selector: 'app-recepie-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recepie-list.component.css']
 })
 export class RecepieListComponent implements OnInit {
-
+recepie:Recepie[]=[
+  new Recepie('new recepie','new description','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDRumNBzv0vfe8OSRpsV_GpiVI0lG0NQXh79V-HnISszRJu9yM'),
+  new Recepie('new recepie','new description','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDRumNBzv0vfe8OSRpsV_GpiVI0lG0NQXh79V-HnISszRJu9yM'),
+]
   constructor() { }
 
   ngOnInit() {
